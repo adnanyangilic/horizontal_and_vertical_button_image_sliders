ab23201ab6e070f99723e4b8a4cc3d053f35806c
adnanyangilic <adnan.alistika@gmail.com>
2022-05-19 12:44:28 +0300

    Proje dosyası ekle.

---
+++ script.js
@@ -0,0 +1,66 @@
+document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/1.jpg)";
+
+document.getElementById("numberbutton1").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/1.jpg)";
+});
+document.getElementById("numberbutton2").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/2.jpg)";
+});
+document.getElementById("numberbutton3").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/3.jpg)";
+});
+document.getElementById("numberbutton4").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/4.jpg)";
+});
+document.getElementById("numberbutton5").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/5.jpg)";
+});
+document.getElementById("numberbutton6").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/6.jpg)";
+});
+document.getElementById("numberbutton7").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/7.jpg)";
+});
+document.getElementById("numberbutton8").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/8.jpg)";
+});
+document.getElementById("numberbutton9").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/9.jpg)";
+});
+document.getElementById("numberbutton10").addEventListener("mouseover", function() {
+    document.getElementById("imageplaceholder").style.backgroundImage = "url(Dove/10.jpg)";
+});
+
+
+document.getElementById("imageplace").style.backgroundImage = "url(Dove/1.jpg)";
+
+document.getElementById("num1").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/1.jpg)";
+});
+document.getElementById("num2").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/2.jpg)";
+});
+document.getElementById("num3").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/3.jpg)";
+});
+document.getElementById("num4").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/4.jpg)";
+});
+document.getElementById("num5").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/5.jpg)";
+});
+document.getElementById("num6").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/6.jpg)";
+});
+document.getElementById("num7").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/7.jpg)";
+});
+document.getElementById("num8").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/8.jpg)";
+});
+document.getElementById("num9").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/9.jpg)";
+});
+document.getElementById("num10").addEventListener("mouseover", function() {
+    document.getElementById("imageplace").style.backgroundImage = "url(Dove/10.jpg)";
+});
